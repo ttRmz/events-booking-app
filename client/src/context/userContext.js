bc919a4a3f071da9bf55604a640fb4819bce75d4
Tristan RAMIREZ <tristan.rami@gmail.com>
2019-04-30 20:28:52 +0200

    feat(userContext): add avatar :art:

--- client/src/context/userContext.js
+++ client/src/context/userContext.js
@@ -1,12 +1,21 @@
+/* eslint-disable no-restricted-globals */
 import React, { createContext, useState, useEffect } from 'react';
 
-const UserContext = createContext({ login: () => {}, logout: () => {}, username: null, userId: null, token: null });
+const userContext = createContext({
+  login: () => {},
+  logout: () => {},
+  username: null,
+  userId: null,
+  token: null,
+  avatar: null
+});
 
 export function UserProvider({ value, ...rest }) {
   const id = localStorage.getItem('userId');
   const tk = localStorage.getItem('token');
   const usr = localStorage.getItem('username');
-  const [user, setUser] = useState({ userId: null, token: null, username: null });
+  const [user, setUser] = useState({ userId: null, token: null, username: null, avatar: null });
+  const getAvatar = id => `https://api.adorable.io/avatars/64/${id}`;
   useEffect(() => {
     if (!tk && user.token) {
       handleStoreUser(user);
@@ -14,28 +23,32 @@ export function UserProvider({ value, ...rest }) {
   }, [user]);
   useEffect(() => {
     if (tk && !user.token) {
-      setUser({ userId: id, token: tk, username: usr });
+      setUser({ userId: id, token: tk, username: usr, avatar: getAvatar(id) });
     }
   }, []);
   const handleLogin = ({ login }) => {
     const { userId, token, pseudo } = login;
-    return setUser({ userId, token, username: pseudo });
+    return setUser({ userId, token, username: pseudo, avatar: getAvatar(userId) });
   };
   const handleStoreUser = user => {
     localStorage.setItem('userId', user.userId);
     localStorage.setItem('token', user.token);
     localStorage.setItem('username', user.username);
+    location.reload();
   };
   const handleLogout = () => {
     localStorage.removeItem('userId');
     localStorage.removeItem('token');
     localStorage.removeItem('username');
-    setUser({ userId: null, token: null, username: null });
+    setUser({ userId: null, token: null, username: null, avatar: null });
   };
   const { userId, token, username } = user;
   return (
-    <UserContext.Provider value={{ username, userId, token, login: handleLogin, logout: handleLogout }} {...rest} />
+    <userContext.Provider
+      value={{ username, userId, token, login: handleLogin, logout: handleLogout, avatar: getAvatar(userId) }}
+      {...rest}
+    />
   );
 }
 
-export default UserContext;
+export default userContext;
