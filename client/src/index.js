3df647da176b4809b934b75574b4250ec5f026c2
Tristan RAMIREZ <tristan.rami@gmail.com>
2019-05-01 14:33:38 +0200

    fix(apollo): store auth token in header

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,36 @@ import ReactDOM from 'react-dom';
 import './sass/index.scss';
 import App from './App';
 import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { UserProvider } from './context/userContext';
 
-const apolloClient = new ApolloClient({
+const httpLink = createHttpLink({
   uri: `http://localhost:3000/graphql`
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <ApolloProvider client={apolloClient}>
-    <App />
-  </ApolloProvider>,
+  <UserProvider>
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </UserProvider>,
   document.getElementById('root')
 );
