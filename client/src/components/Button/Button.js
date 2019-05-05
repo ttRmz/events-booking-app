0165cecbbe3923fe00e9da65b7344d37ccf487e5
Tristan RAMIREZ <tristan.rami@gmail.com>
2019-05-05 23:02:12 +0200

    feat(btn): add active props ✅

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './Button.scss';
 import classNames from 'classnames';
 
-function Button({ variant, color, className, component, href, ...rest }) {
+function Button({ variant, color, className, component, active, href, ...rest }) {
   const Component = component ? component : href ? 'a' : 'button';
-  const btnClass = classNames('btn', className, `btn--${variant}--${color}`);
+  const btnClass = classNames('btn', className, `btn--${variant}--${color}`, {
+    [`btn--active`]: active
+  });
   return <Component href={href} className={btnClass} {...rest} />;
 }
 
